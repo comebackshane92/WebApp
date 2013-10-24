c98c91027a7cd92f8f665afdf7a8d4672e353a17
Shane Svenpladsen <shane.t.svenpladsen@vanderbilt.edu>
2013-10-23 20:08:04 -0500

    Creating Home and About pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 WebApp::Application.routes.draw do
+  root "pages#home" #sets homepage
+  get "about" => "pages#about" #creates about_path
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
